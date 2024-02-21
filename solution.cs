3c3b95c6a47475f2261989b1d7b10bb9947078e0
Kooreis <kooreis.official@gmail.com>
2024-02-21 17:24:45 -0600

    Added test cases to the Main method to test the IsMatch function.

--- solution.cs
+++ solution.cs
@@ -4,5 +4,10 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine(IsMatch("aa", "a"));  // false
+        Console.WriteLine(IsMatch("aa", "a*"));  // true
+        Console.WriteLine(IsMatch("ab", ".*"));  // true
+        Console.WriteLine(IsMatch("aab", "c*a*b"));  // true
+        Console.WriteLine(IsMatch("mississippi", "mis*is*p*."));  // false
     }
 }
